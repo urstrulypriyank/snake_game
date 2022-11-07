5652bf678d9b1e867d346c49a685834a7f4907cf
priyank rai <0priyankrai@gmail.com>
2022-11-07 20:29:31 +0530

    basic functionality achieved

--- snake_game.py
+++ snake_game.py
@@ -12,6 +12,7 @@ WHITE,BLACK,RED = (255,255,255),(0,0,0),(255,0,0)
 
 # GAME SPECIFIC VARIABLE 
 EXIT_GAME,QUIT_GAME = False,False
+score = 0
 
 
 
@@ -33,15 +34,18 @@ def engine_logic(event):
 
 def main():
     # IMPORTING SOME GLOABL VARIABLES
-    global EXIT_GAME,WHITE
+    global EXIT_GAME,WHITE,score
 
     # GAME LOOP
     set_backgroudColor(WHITE)
     global GAMEWINDOW,BLACK,FPS,EXIT_GAME
     snake_x,snake_y,snake_size = 45,55,10
+    ssize = 10
+    snake_sizex = 10
+    snake_sizey = 10
     velocity_x = velocity_y = 0
-    foodx = random.randint(0, WINDOW_WIDTH)
-    foody = random.randint(0, WINDOW_HEGIHT)
+    foodx = random.randint(20, WINDOW_WIDTH/2)
+    foody = random.randint(20, WINDOW_HEGIHT/2)
     clock = pygame.time.Clock()
     
     while not EXIT_GAME:
@@ -53,17 +57,27 @@ def main():
                 if event.key == pygame.K_RIGHT:
                     velocity_x = 10
                     velocity_y =0
+                    snake_sizex = ssize
+                    snake_sizey = 10
+
+
                    
                 if event.key == pygame.K_LEFT:
                     velocity_x = -10
                     velocity_y = 0
+                    snake_sizex = ssize
+                    snake_sizey = 10
                 if event.key == pygame.K_DOWN :
                     velocity_y = 10
                     velocity_x = 0
+                    snake_sizey = ssize
+                    snake_sizex = 10
                 if event.key == pygame.K_UP:
                     print("UP")
                     velocity_y = -10
                     velocity_x = 0
+                    snake_sizey = ssize
+                    snake_sizex = 10
                
                 
                 print(snake_x,snake_y)
@@ -77,9 +91,15 @@ def main():
         if(snake_y >WINDOW_HEGIHT-5):
             snake_y = 5
         snake_x += velocity_x
-        snake_y += velocity_y        
+        snake_y += velocity_y  
+        if abs(snake_x -foodx)<10 and abs(snake_y-foody)<10:
+            score+=1
+            foodx = random.randint(20, WINDOW_WIDTH/2)
+            foody = random.randint(20, WINDOW_HEGIHT/2)
+            ssize+=10
+            print("Score",score)      
         GAMEWINDOW.fill(WHITE)
-        pygame.draw.rect(GAMEWINDOW, BLACK, [snake_x,snake_y,snake_size,snake_size])
+        pygame.draw.rect(GAMEWINDOW, BLACK, [snake_x,snake_y,snake_sizex,snake_sizey])
         pygame.draw.rect(GAMEWINDOW, RED, [foodx,foody,snake_size,snake_size])
         pygame.display.update()
         clock.tick(FPS) 
